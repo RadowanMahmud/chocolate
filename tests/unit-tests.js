f2d660485189e2bf95efbc7e19de9928b70b8419
RadowanMahmud <bsse1002@iit.du.ac.bd>
2023-05-24 23:38:58 +0600

    developed unit testing for both solutions end point and functions

--- tests/unit-tests.js
+++ tests/unit-tests.js
@@ -1,22 +1,55 @@
 const axios = require("axios");
 const {expect} = require("chai");
 var type = ["milk", "white", "sf", "dark"];
+const logics = require("../src/logicalfunctions");
+const cases = require("./case");
 
-describe("Comparing both solutions", async () => {
-    it("Comparing both solutions", async () => {
-        var body = {
-            "cash": 6,
-            "price": 2,
-            "wrapper_needed": 2,
-            "type": type[Math.floor(Math.random() / 4)]
-        }
-        const res = await axios.post("http://localhost:3000/", body);
-        const resStable = await axios.post("http://localhost:3000/stable", body);
-        console.log(body)
-        console.log(res.data)
-        console.log(resStable.data)
-        for (let t of type) {
-            expect(res.data[t]).equal(resStable.data[t]);
-        }
+for(let c of cases.case){
+    describe("Testing solutions functions with "+c[0], async () => {
+        it('O(N)', function () {
+            var ans = logics.offerRedemtionRateInOofN(c[0][0],c[0][1],c[0][2],c[0][3]);
+            for (let t of type) {
+                expect(ans[t]).equal(c[1][t]);
+            }
+        });
+        it('O(1)', function () {
+            var ans = logics.offerRedemtionRateInOofONE(c[0][0],c[0][1],c[0][2],c[0][3]);
+            for (let t of type) {
+                expect(ans[t]).equal(c[1][t]);
+            }
+        });
     })
-})
+
+    describe("Comparing both solutions "+c[0], async () => {
+        it("Comparing both solutions", async () => {
+            var body = {
+                "cash": c[0][0],
+                "price": c[0][1],
+                "wrapper_needed": c[0][2],
+                // "type": type[Math.floor(Math.random() / 4)]
+                "type": c[0][3]
+            }
+            const res = await axios.post("http://localhost:3000/", body);
+            const resStable = await axios.post("http://localhost:3000/stable", body);
+            console.log(res.data)
+            for (let t of type) {
+                expect(res.data[t]).equal(resStable.data[t]);
+            }
+        })
+    })
+    describe("Comparing both solutions "+c[0]+" but with random type", async () => {
+        it("Comparing both solutions", async () => {
+            var body = {
+                "cash": c[0][0],
+                "price": c[0][1],
+                "wrapper_needed": c[0][2],
+                "type": type[Math.floor(Math.random() / 4)]
+            }
+            const res = await axios.post("http://localhost:3000/", body);
+            const resStable = await axios.post("http://localhost:3000/stable", body);
+            for (let t of type) {
+                expect(res.data[t]).equal(resStable.data[t]);
+            }
+        })
+    })
+}
